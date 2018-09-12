97c42e276943bdd8cd9d4a370485906be025326e
ianshiundu <ian.shiundu@gmail.com>
2018-09-12 11:48:26 +0300

    tests preparation

---
+++ src/test/scala/com/showtix/StopSystemAfterAll.scala
@@ -0,0 +1,14 @@
+package com.showtix
+
+import akka.testkit.TestKit
+
+import org.scalatest.{Suite, BeforeAndAfterAll}
+
+trait StopSystemAfterAll extends BeforeAndAfterAll {
+  this: TestKit with Suite =>
+  override protected def afterAll(): Unit = {
+    super.afterAll()
+    system.terminate()
+  }
+}
+
